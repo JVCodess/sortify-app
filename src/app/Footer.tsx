9709562b98cf2fa9fbf72b936d80aac1382b476a
Joshua Valera <156018264+JVCodess@users.noreply.github.com>
2025-05-02 15:11:59 -0700

    Create Footer.tsx

---
+++ src/app/Footer.tsx
@@ -0,0 +1,13 @@
+// src/app/footer.tsx
+const Footer = () => {
+    return (
+      <footer className="py-16 px-4 max-w-full mx-auto text-center bg-[#121212] font-press-start-2p">
+        <p className="text-sm text-gray-400">
+          © {new Date().getFullYear()} Joshua Valera | 2025 ASU
+        </p>
+      </footer>
+    );
+  };
+  
+  export default Footer;
+  
